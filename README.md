8357f066abc030e62a7fe6f74f83a055667b7959
Koen De Vleeschauwer <kdv@kdvelectronics.eu>
2018-01-18 14:45:57 -0500

    binary

--- README.md
+++ README.md
@@ -1,37 +1,28 @@
-[FFmpeg-Android](http://hiteshsondhi88.github.io/ffmpeg-android/) [![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-FFmpeg--Android-brightgreen.svg?style=flat)](https://android-arsenal.com/details/1/925)
-==============
 
-* FFmpeg for Android compiled with x264, libass, fontconfig, freetype and fribidi
-* Supports Android L
-* [FFmpeg Android Library](https://github.com/hiteshsondhi88/ffmpeg-android-java)
+* FFmpeg for Android compiled without text relocations. 
+* For use with TarsosDSP; audio support only.
+* Based upon [FFmpeg Android](https://github.com/hiteshsondhi88/ffmpeg-android)
 
 Supported Architecture
 ----
 * armv7
 * armv7-neon
-* x86
 
 Instructions
 ----
+* Clone git repository
+  git clone --recursive https://github.com/koendv/FFmpeg-Audio-Android
 * Set environment variable
-  1. export ANDROID_NDK={Android NDK Base Path}
+  export ANDROID_NDK={Android NDK Base Path}
+  Example:
+  export ANDROID_NDK=/opt/android-ndk-r10e/
 * Run following commands to compile ffmpeg
-  1. sudo apt-get --quiet --yes install build-essential git autoconf libtool pkg-config gperf gettext yasm python-lxml
-  2. ./init_update_libs.sh
-  3. ./android_build.sh
-* To update submodules and libraries you can use ./init_update_libs.sh command
+  sudo apt-get --quiet --yes install build-essential git autoconf libtool pkg-config gperf gettext yasm python-lxml
+  ./android_build.sh
 * Find the executable binary in build directory.
-* If you want to use FONTCONFIG then you need to specify your custom fontconfig config file (e.g - "FONTCONFIG_FILE=/sdcard/fonts.conf ./ffmpeg --version", where /sdcard/fonts.conf is location of your FONTCONFIG configuration file).
-* You can also download [prebuilt-binaries](https://github.com/hiteshsondhi88/ffmpeg-android/releases/latest) (Releases &gt; Downloads &gt; prebuilt-binaries[zip|tar.gz]).
+* You can also download [prebuilt-binaries](https://github.com/koendv/FFmpeg-Audio-Android/blob/master/release/ffmpeg_android_binary.zip)
 
 License
 ----
-  check files LICENSE.GPLv3 and LICENSE
-
-HIRE US
---------
-Get in touch with us - http://www.writingminds.com
-
-
-[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/hiteshsondhi88/ffmpeg-android/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+  See files LICENSE.GPLv3 and LICENSE
 
--- ffmpeg_build.sh
+++ ffmpeg_build.sh
@@ -11,6 +11,9 @@ case $1 in
   x86)
     CPU='i686'
   ;;
+  x86_64)
+    CPU='x86_64'
+  ;;
 esac
 
 make clean
@@ -23,14 +26,17 @@ make clean
 --enable-runtime-cpudetect \
 --sysroot="$NDK_SYSROOT" \
 --enable-pic \
---enable-libx264 \
---enable-libass \
---enable-libfreetype \
---enable-libfribidi \
---enable-libmp3lame \
---enable-fontconfig \
+--disable-libx264 \
+--disable-libass \
+--disable-libfreetype \
+--disable-libfribidi \
+--disable-libmp3lame \
+--disable-fontconfig \
 --enable-pthreads \
 --disable-debug \
+--enable-rdft \
+--disable-avdevice \
+--disable-swscale \
 --disable-ffserver \
 --enable-version3 \
 --enable-hardcoded-tables \
@@ -41,11 +47,31 @@ make clean
 --disable-doc \
 --disable-shared \
 --enable-static \
+\
+--disable-protocols \
+--enable-protocol='file,pipe,tcp' \
+--disable-demuxers \
+--disable-muxers \
+--enable-demuxer='aac,aiff,matroska,mp3,ogg,pcm_alaw,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_mulaw,pcm_s16be,pcm_s16le,pcm_s24be,pcm_s24le,pcm_s32be,pcm_s32le,pcm_s8,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,pcm_u32be,pcm_u32le,pcm_u8,wav' \
+--enable-muxer='aiff,flac,matroska,matroska_audio,mp3,oga,ogg,opus,pcm_alaw,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_mulaw,pcm_s16be,pcm_s16le,pcm_s24be,pcm_s24le,pcm_s32be,pcm_s32le,pcm_s8,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,pcm_u32be,pcm_u32le,pcm_u8,wav' \
+--disable-filters \
+--enable-filter='abuffer,abuffersink,acompressor,acontrast,acopy,acrossfade,acrusher,adelay,aecho,aemphasis,aeval,aevalsrc,afade,afftfilt,afir,aformat,agate,aiir,alimiter,allpass,aloop,amerge,amix,anequalizer,anoisesrc,anull,anullsink,anullsrc,apad,aphaser,apulsator,aresample,areverse,asetnsamples,asetrate,ashowinfo,astats,atempo,atrim,bandpass,bandreject,bass,biquad,bs2b,channelmap,channelsplit,chorus,compand,compensationdelay,crossfeed,crystalizer,dcshift,dynaudnorm,earwax,equalizer,extrastereo,firequalizer,flanger,flite,haas,hdcd,headphone,highpass,hilbert,join,ladspa,loudnorm,lowpass,lv2,mcompand,pan,replaygain,resample,rubberband,sidechaincompress,sidechaingate,silencedetect,silenceremove,sine,sofalizer,stereotools,stereowiden,superequalizer,surround,treble,tremolo,vibrato,volume,volumedetect' \
+--disable-parsers \
+--enable-parser='aac,aac_latm,flac,vorbis' \
+--disable-encoders \
+--disable-decoders \
+--enable-encoder='aac,aac_at,alac,alac_at,flac,libmp3lame,libopus,libshine,libvorbis,opus,pcm_alaw,pcm_alaw_at,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_mulaw,pcm_mulaw_at,pcm_s16be,pcm_s16be_planar,pcm_s16le,pcm_s16le_planar,pcm_s24be,pcm_s24daud,pcm_s24le,pcm_s24le_planar,pcm_s32be,pcm_s32le,pcm_s32le_planar,pcm_s64be,pcm_s64le,pcm_s8,pcm_s8_planar,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,pcm_u32be,pcm_u32le,pcm_u8,wmav1,wmav2' \
+--enable-decoder='aac,aac_at,aac_fixed,aac_latm,alac,alac_at,flac,libopus,libvorbis,mp3,mp3_at,mp3adu,mp3adufloat,mp3float,mp3on4,mp3on4float,pcm_alaw,pcm_alaw_at,pcm_f16le,pcm_f24le,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_lxf,pcm_mulaw,pcm_mulaw_at,pcm_s16be,pcm_s16be_planar,pcm_s16le,pcm_s16le_planar,pcm_s24be,pcm_s24daud,pcm_s24le,pcm_s24le_planar,pcm_s32be,pcm_s32le,pcm_s32le_planar,pcm_s64be,pcm_s64le,pcm_s8,pcm_s8_planar,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,pcm_u32be,pcm_u32le,pcm_u8,wmalossless,wmapro,wmav1,wmav2,wmavoice' \
+--disable-bsfs \
+--disable-indevs \
+--disable-outdevs \
+--disable-devices \
+\
 --pkg-config="${2}/ffmpeg-pkg-config" \
 --prefix="${2}/build/${1}" \
 --extra-cflags="-I${TOOLCHAIN_PREFIX}/include $CFLAGS" \
 --extra-ldflags="-L${TOOLCHAIN_PREFIX}/lib $LDFLAGS" \
---extra-libs="-lpng -lexpat -lm" \
+--extra-libs="-lm" \
 --extra-cxxflags="$CXX_FLAGS" || exit 1
 
 make -j${NUMBER_OF_CORES} && make install || exit 1
